591487f4793610d76ca185cdb2273d5510058bad
Miley <miley.ho@hyweb.com.tw>
2021-04-23 11:33:04 +0800

    調整adSlider顯示數量

--- js/customize.js
+++ js/customize.js
@@ -41,7 +41,7 @@ $(function() {
         responsive: [{
             breakpoint: 1200,
             settings: {
-                slidesToShow: 5,
+                slidesToShow: 4,
                 slidesToScroll: 1,
                 arrows: true
             }
